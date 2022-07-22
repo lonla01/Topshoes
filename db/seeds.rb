104908a75bd82408ab1dbc1797765847261f22ef
Serge P. LONLA <splonla@gmail.com>
2022-07-22 16:09:25 +0100

    Try fixing a deployment bug. 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,8 +44,8 @@ class Seed
   def self.load_path
     # path = "/Users/patrice/Desktop/Developer/Projects/Topshoes/app/assets/images/Babouches1"
     # file_dir = File.dirname(__FILE__)
-    file_dir = Rails.root.join('assets', 'images')
-    puts "Rails.root = #{Rails.root.to_s}"
+    file_dir = Rails.root.join('app', 'assets', 'images')
+    puts "Rails.root = #{Rails.root}"
     puts "file_dir = #{file_dir}"
     file_dir
   end
