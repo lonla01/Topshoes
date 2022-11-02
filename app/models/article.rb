953091142a7cafc2af7f6ad5154f2f950b4b9f94
Patrice LONLA <splonla@gmail.com>
2022-11-02 18:09:14 +0100

    Deployment 3

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,8 +9,8 @@ class Article < ApplicationRecord
   def img_loc
     basename = Pathname.new(img).basename
     img_path = Pathname.new(category).join(basename)
-    img_url = "#{Article.local_base_url}/#{img_path}"
-    img_url
+    #img_url = "#{Article.local_base_url}/#{img_path}"
+    img_path
   end
 
   def self.local_base_url
