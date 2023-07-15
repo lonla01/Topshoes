50fb7c7e64e703b7ae7ea08ca24d5a3cd63d2819
Serge P. LONLA <splonla@gmail.com>
2023-07-14 21:05:07 -0700

    Fix a bug that occured while importing React in the project.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,16 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link fontawesome
+//= link cloudflare.all.min
+//= link tailwind
+
+//= link flex-slider.css
+//= link bootstrap.min.css
+//= link templatemo-sixteen.css
+//= link owl
+//= link styles.css
+//= link slick
+//= link slick-theme.css
+//= link owl
+//= link isotope
